7f26a80ab7129462a181958ea1e4405b26ad4f29
nishanprime <thapanishan9@gmail.com>
2021-04-23 23:18:47 -0400

    added listener and get

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
 import express from 'express';
+
+const app = express();
+
+//port
+const PORT = process.env.PORT || 3000;
+
+//get route
+app.get('/', (req, res) => {
+  res.send('Bipin is very good');
+});
+
+//listener
+
+app.listen(PORT, () => {
+  console.log('Bipin is gay');
+});
